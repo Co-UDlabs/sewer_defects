37cb67170f9726134fb0e352b7dbac5b093a4ddc
Ehsan <e.kazemi@sheffield.ac.uk>
2023-03-17 23:39:14 +0000

    Test prediction on new images

--- coudlabs/codes/predict.py
+++ coudlabs/codes/predict.py
@@ -1,19 +1,25 @@
 from ultralytics import YOLO
 import cv2
+import os
 
 # best model and data paths
-bestModelPath = "C:\Ehsan\\sewer_defects\\coudlabs\\runs\\detect\\train\\weights\\best.pt"
-imagePath = "C:\\Ehsan\\sewer_defects\\coudlabs\\prediction\\01\\obstacle_test.jpg"
+bestModelPath = "C:\Ehsan\\sewer_defects\\coudlabs\\runs\\detect\\train2\\weights\\best.pt"
+imageFolder = "C:\\Ehsan\\sewer_defects\\coudlabs\\prediction\\coudlabs01\\"
 
 # load model
 bestModel = YOLO(bestModelPath)
 
-# get result
-img = cv2.imread(imagePath)
-result = bestModel(img)  # predict on an image
-#print(result[0])
-
-# plot result
-res_plotted = result[0].plot()
-cv2.imshow("result", res_plotted)
-cv2.waitKey(0)
+# loop on images
+for filename in os.listdir(imageFolder):
+    img = cv2.imread(os.path.join(imageFolder,filename))
+    if img is not None:
+        # get result
+        result = bestModel(img)  # predict on an image
+        #print(result[0])
+        # plot result
+        resImg = result[0].plot()
+        cv2.imshow("result", resImg)
+        cv2.waitKey(1)
+        # write result
+        fname, fext = os.path.splitext(filename)
+        cv2.imwrite(imageFolder+fname+'_out'+fext,resImg)

a5c806137a2b9bf67f4b72879b67bda884abd99b
Ehsan <e.kazemi@sheffield.ac.uk>
2023-03-18 01:24:07 +0000

    Test prediction model for videos

--- coudlabs/codes/predict.py
+++ coudlabs/codes/predict.py
@@ -9,17 +9,9 @@ imageFolder = "C:\\Ehsan\\sewer_defects\\coudlabs\\prediction\\coudlabs01\\"
 # load model
 bestModel = YOLO(bestModelPath)
 
-# loop on images
+# loop on images & videos in imageFolder
 for filename in os.listdir(imageFolder):
-    img = cv2.imread(os.path.join(imageFolder,filename))
+    img = imageFolder+filename
     if img is not None:
         # get result
-        result = bestModel(img)  # predict on an image
-        #print(result[0])
-        # plot result
-        resImg = result[0].plot()
-        cv2.imshow("result", resImg)
-        cv2.waitKey(1)
-        # write result
-        fname, fext = os.path.splitext(filename)
-        cv2.imwrite(imageFolder+fname+'_out'+fext,resImg)
+        result = bestModel(img, save=True)

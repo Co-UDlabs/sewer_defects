aad412c5390c532937b716e4fb2c2078c63f4c64
Ehsan <e.kazemi@sheffield.ac.uk>
2023-04-26 21:04:55 +0100

    Split data and copy from a folder into multiple folders

--- coudlabs/codes/prepare_data.py
+++ coudlabs/codes/prepare_data.py
@@ -1,18 +1,24 @@
 import os
+import glob
 import random
 import shutil
+
+# name of dataset
+data_name = "data_01"
+
+# Set the paths for the source folder and the three destination folders
+source_path = 'C:/Ehsan/sewer_data/labelled images'
+
+# file extensions
+extensions = ["png", "jpg", "tif", "bmp"]
 
 # Set the percentages for each subset (should add up to 100)
 subset1_percent = 70
 subset2_percent = 15
 subset3_percent = 15
 
-# Set the paths for the source folder and the three destination folders
-source_images = 'C:/Ehsan/sewer_data/images/'
-source_labels = 'C:/Ehsan/sewer_data/labels/'
-
 # Set the local paths for the three folders where the downloaded files will be saved
-destination_dir = "C:/Ehsan/sewer_defects/coudlabs/data/test_01"
+destination_dir = "C:/Ehsan/sewer_defects/coudlabs/data/" + data_name
 train_images = destination_dir + "/images/training/"
 train_labels = destination_dir + "/labels/training/"
 valid_images = destination_dir + "/images/validation/"
@@ -23,43 +29,42 @@ test_labels = destination_dir + "/labels/test/"
 # Destination folders (to be overwritten))
 for folder in [train_images,valid_images,test_images,train_labels,valid_labels,test_labels]:
     if os.path.exists(folder):
-        os.remove(folder)
+        shutil.rmtree(folder)
     os.makedirs(folder)
 
-# Get the list of files in the source folder
-file_list = os.listdir(source_images)
+# List of image files in source path
+image_list = []
+for ext in extensions:
+    image_list.extend(glob.glob(os.path.join(source_path, f'*.{ext}')))
+
+# File names
+file_names = [os.path.splitext(file)[0] for file in image_list]
 
 # Calculate the number of files for each subset based on the percentages
-nsub1 = int(len(file_list) * subset1_percent / 100)
-nsub2 = int(len(file_list) * subset2_percent / 100)
-nsub3 = int(len(file_list) * subset3_percent / 100)
+nsub1 = int(len(image_list) * subset1_percent / 100)
+nsub2 = int(len(image_list) * subset2_percent / 100)
+nsub3 = int(len(image_list) * subset3_percent / 100)
 
-# Shuffle the list of files
-random.shuffle(file_list)
+# Splid images intwo three subsets (training / validation / test) randomly
+random.shuffle(image_list)
+files_subset1 = image_list[0:nsub1]
+files_subset2 = image_list[nsub1:nsub1+nsub2]
+files_subset3 = image_list[nsub1+nsub2:nsub1+nsub2+nsub3]
 
-# 
-files_subset1 = file_list[0:nsub1]
-files_subset2 = file_list[nsub1:nsub1+nsub2]
-files_subset3 = file_list[nsub1+nsub2:nsub1+nsub2+nsub3]
+# Function to copy images and labels to the destination folders
+def copy_files(image_files,image_folder,label_folder):
+    missing_text_files = []
+    for file in image_files:
+        shutil.copy(os.path.join(source_path, file), image_folder)
+        text_file = os.path.splitext(file)[0] + ".txt"
+        if os.path.isfile(os.path.join(source_path, text_file)):
+            shutil.copy(os.path.join(source_path, text_file), label_folder)
+        else:
+            missing_text_files.append(text_file)
+    if missing_text_files:
+        raise Exception("ERROR: the following label files are missing: ", missing_text_files)
 
-# Copy images to the destination folders based on the subset percentages
-for i, file in enumerate(files_subset1):
-    shutil.copy(os.path.join(source_images, file), train_images)
-for i, file in enumerate(files_subset2):
-    shutil.copy(os.path.join(source_images, file), valid_images)
-for i, file in enumerate(files_subset3):
-    shutil.copy(os.path.join(source_images, file), test_images)
-
-# Copy labels
-l = [os.path.splitext(x)[0] for x in files_subset1]
-l = [x + '.txt' for x in l]
-for i, file in enumerate(l):
-    shutil.copy(os.path.join(source_labels, file), train_labels)
-l = [os.path.splitext(x)[0] for x in files_subset2]
-l = [x + '.txt' for x in l]
-for i, file in enumerate(l):
-    shutil.copy(os.path.join(source_labels, file), valid_labels)
-l = [os.path.splitext(x)[0] for x in files_subset3]
-l = [x + '.txt' for x in l]
-for i, file in enumerate(l):
-    shutil.copy(os.path.join(source_labels, file), test_labels)
+# Copy files
+copy_files(files_subset1,train_images,train_labels)
+copy_files(files_subset2,valid_images,valid_labels)
+copy_files(files_subset3,test_images,test_labels)

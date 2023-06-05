addb4d5ab11adab3a352e96a8488a60068a89e18
Ehsan <e.kazemi@sheffield.ac.uk>
2023-06-06 00:03:36 +0100

    Modify and comment

--- coudlabs/src/utils/distance_and_size.py
+++ coudlabs/src/utils/distance_and_size.py
@@ -1,112 +1,58 @@
-'''
-The code reads the data from the text files, compares the sizes of the 
-two objects in each frame (where both objects are present), calculates 
-the distance of the reference object (usually a joint) from it's real 
-size (m) and its size on the image (pixels), then calculates change in 
-the image sizes of both objects, calculates the ratio between the change
-in the image sizes, an finally estimates the distance of the second 
-object from the camera as well as its size.
-'''
+"""
+File name: distance_and_size.py
+Author: Ehsan Kazemi
+Date created: Apr 2023
+Date last modified: 05/06/2023
+-------------------------------------------------------------------------\
+This code calculates the size ratios, distances, and real sizes of       /
+objects based on the reference object's height and the camera's focal    \
+length.                                                                  /
+-------------------------------------------------------------------------\
+"""
 
 def distsize_I(data_ref, data_other, real_size_ref, focal_length):
+    """
+    Parameters:
+    - data_ref: The data of the reference object.
+    - data_other: The data of the other object.
+    - real_size_ref: The real size of the reference object in meters.
+    - focal_length: The focal length of the camera in pixels.
 
-    # Extract the frame numbers from each file
-    frame_numbers_ref = set(int(row[0]) for row in data_ref)
-    frame_numbers_other = set(int(row[0]) for row in data_other)
-
-    # initialise
-    image_size_other_prev = None
-    image_size_ref_prev = None
+    Returns:
+    - distance_ref_list: A list of estimated distances of the reference object to the camera.
+    - distance_other_list: A list of estimated distances of the other object to the camera.
+    - real_size_other_list: A list of estimated real sizes of the other object.
+    - common_frame_numbers: A list of frame numbers where both objects are present.
+    """
+    # Initialize lists to store the calculated values
     distance_ref_list = []
     distance_other_list = []
     real_size_other_list = []
-    
-    # Find the common frame numbers
-    common_frame_numbers = frame_numbers_ref.intersection(frame_numbers_other)
-    
-    # Iterate over the common frame numbers
-    for frame_number in common_frame_numbers:
-        # Find the rows with the matching frame number in each file
-        matching_rows_ref = [row for row in data_ref if int(row[0]) == frame_number]
-        matching_rows_other = [row for row in data_other if int(row[0]) == frame_number]
+    common_frame_numbers = []
 
-        # Perform operations on the matching rows
-        for ref, other in zip(matching_rows_ref, matching_rows_other):
-            
-            # Extract the relevant information from the rows and perform operations
-            image_size_ref = float(ref[4])
-            image_size_other = float(other[4])
+    # Iterate over the data rows
+    for i, row_ref in enumerate(data_ref):
+        # Get the frame number and the reference object's height
+        frame_number_ref = int(row_ref[0])
+        height_ref = float(row_ref[4])
 
-            # ref real distance
-            distance_ref = (real_size_ref * focal_length) / image_size_ref
-            distance_ref_list.append(distance_ref)
+        # Search for the corresponding row in the other object's data
+        for j, row_other in enumerate(data_other):
+            frame_number_other = int(row_other[0])
 
-            if image_size_ref_prev is not None and image_size_other_prev is not None:
+            # If the frame numbers match, calculate the size ratios, distances, and real sizes
+            if frame_number_ref == frame_number_other:
+                height_other = float(row_other[4])
+                size_ratio = height_other / height_ref
+                distance_ref = real_size_ref * focal_length / height_ref
+                distance_other = distance_ref * size_ratio
+                real_size_other = real_size_ref * size_ratio
 
-                # image size change for ref and other object
-                image_size_change_ref = image_size_ref - image_size_ref_prev
-                image_size_change_other = image_size_other - image_size_other_prev
-            
-                if image_size_change_ref != 0 and image_size_change_other != 0:
-                    # ratio of image size change
-                    image_size_change_ratio = image_size_change_ref / image_size_change_other
-            
-                    # estimate distance of other object to camera
-                    distance_other = distance_ref * image_size_change_ratio
-                    distance_other_list.append(distance_other)
-            
-                    # estimate real size of other object
-                    real_size_other = distance_other * image_size_other / focal_length
-                    real_size_other_list.append(real_size_other)
-
-            # set current as prev for next interation
-            image_size_ref_prev = image_size_ref
-            image_size_other_prev = image_size_other
-
-    return distance_ref_list, distance_other_list, real_size_other_list, common_frame_numbers
-
-
-
-def distsize_II(data_ref, data_other, real_size_ref, focal_length):
-    frame_numbers_ref = set(int(row[0]) for row in data_ref)
-    frame_numbers_other = set(int(row[0]) for row in data_other)
-    image_size_other_prev = None
-    image_size_ref_prev = None
-    distance_ref_list = []
-    distance_other_list = []
-    real_size_other_list = []
-    common_frame_numbers = frame_numbers_ref.intersection(frame_numbers_other)
-    
-    for frame_number in common_frame_numbers:
-        matching_rows_ref = [row for row in data_ref if int(row[0]) == frame_number]
-        matching_rows_other = [row for row in data_other if int(row[0]) == frame_number]
-
-        if matching_rows_other:  # Check if matching_rows_other is not empty
-            for ref, other in zip(matching_rows_ref, matching_rows_other):
-                image_size_ref = float(ref[4])
-                image_size_other = float(other[4])
-
-                distance_ref = focal_length * real_size_ref / image_size_ref
                 distance_ref_list.append(distance_ref)
+                distance_other_list.append(distance_other)
+                real_size_other_list.append(real_size_other)
+                common_frame_numbers.append(frame_number_ref)
 
-                if image_size_ref_prev is not None and image_size_other_prev is not None:
-                    image_size_diff_ref = 1 / image_size_ref_prev - 1 / image_size_ref
-                    image_size_diff_other = 1 / image_size_other_prev - 1 / image_size_other
-
-                    if image_size_diff_other != 0:
-                        real_size_other = real_size_ref * image_size_diff_ref / image_size_diff_other
-                        real_size_other_list.append(real_size_other)
-                        distance_other = focal_length * real_size_other / image_size_other
-                        distance_other_list.append(distance_other)
-                    else:
-                        real_size_other_list.append(None)
-                        distance_other_list.append(None)
-                
-                image_size_ref_prev = image_size_ref
-                image_size_other_prev = image_size_other
-        else:  # Append None values to maintain indices
-            distance_ref_list.append(None)
-            distance_other_list.append(None)
-            real_size_other_list.append(None)
+                break
 
     return distance_ref_list, distance_other_list, real_size_other_list, common_frame_numbers
